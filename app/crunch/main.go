cca9f65a14b185b802cd7d059fd856854a4d9d87
Michael Graff <explorer@flame.org>
2022-05-06 14:37:42 -0500

    start on API

--- app/crunch/main.go
+++ app/crunch/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"math/big"
+	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/host"
@@ -15,41 +15,122 @@ var (
 	three = big.NewInt(3)
 )
 
-func cpuinfo() {
-	cpus, err := cpu.Info()
-	if err != nil {
-		log.Fatal(err)
-	}
-	j, err := json.Marshal(cpus)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("CPU Info: %s", string(j))
+// NodeInfo holds some somewhat arbitrary info about a worker node.
+type NodeInfo struct {
+	HostInfo host.InfoStat  `json:"hostInfo,omitempty"`
+	CPUInfo  []cpu.InfoStat `json:"cpuInfo,omitempty"`
+	Workers  int            `json:"workers,omitempty"`
 }
 
-func showHostname() {
-	info, err := host.Info()
+// WorkPacket is a message from the server to incidate a work
+// item.
+type WorkPacket struct {
+	// ID is the work packet ID, assigned by the server.
+	ID string `json:"id,omitempty"`
+
+	// Nonce is used as a work authenticator.
+	Nonce string `json:"nonce,omitempty"`
+
+	// AssignedOn is when this work item was assigned.
+	AssignedOn time.Time `json:"assignedOn,omitempty"`
+
+	// Expiry indicates a cutoff time after which
+	// this work item may be considered abandoned, and reassigned.
+	// This is not exactly a hard cut-off, and if the work is
+	// completed after this time, if the evidence is accepted,
+	// work will still be considered complete.
+	Expiry time.Time `json:"expiry,omitempty"`
+
+	// StartingValue is the first number (inclusive) to check.
+	StartingValue *big.Int `json:"startingValue,omitempty"`
+
+	// EndingValue is the last number (inclusive) to check.
+	EndingValue *big.Int `json:"endingValue,omitempty"`
+}
+
+// WorkProgressReport is a message sent to indicate
+// completed work, as well as status updates as work is
+// performed, and other status changes.
+type WorkProgressReport struct {
+	// ID is the work packet ID, assigned by the server.
+	ID string `json:"id,omitempty"`
+
+	// Nonce is used as a work authenticator.
+	Nonce string `json:"nonce,omitempty"`
+
+	// StartingValue is the first number (inclusive) to check.
+	StartingValue *big.Int `json:"startingValue,omitempty"`
+
+	// EndingValue is the last number (inclusive) to check.
+	EndingValue *big.Int `json:"endingValue,omitempty"`
+
+	// NodeInfo is the collected node info for where this work
+	// was performed.
+	NodeInfo NodeInfo `json:"nodeInfo,omitempty"`
+
+	// WorkerID is the specific worker thread which completed this
+	// work unit.
+	WorkerID int `json:"workerID,omitempty"`
+
+	// Evidence is a base64(sha512("Work.ID:Work.Nonce:StartingValue:EndingValue:$iterHash"))
+	// where $iterHash is a hash of the string representation of each iteration count, with a single \n
+	// between them.
+	// If the work is still in progress, this should be empty.
+	Evidence string `json:"evidence,omitempty"`
+
+	// Authenticator is a signed Evidence hash, which is
+	//   auth = sha512(UserID:UserSecret:UserSecretVersion:Evidence)
+	// base64(sha512(SecretVersion + ":" + base64(auth)))
+	// If the work is still in progress, the hash is of an evidence
+	// hash where $iterString is set to "in-progress"
+	Authenticator string
+
+	// Status indicates why we are sending this report.
+	//   PENDING = in our work list, but not yet started.
+	//   RUNNING = currently running on a worker.
+	//   ABANDONED = we no longer wish to work on this.
+	//   COMPLETED = we have completed the work requested.
+	// While statuses other than "COMPLETED" can be sent and will
+	// update the user's view of work they have in progress,
+	// only "COMPLETED" is required to be sent.  Work without
+	// any other update will be marked as "PENDING" in the UI.
+	Status string
+
+	// StartedOn is the UTC timestamp of when we began working on this specific work packet.
+	StartedOn time.Time `json:"startedOn,omitempty"`
+
+	// CompletedOn is when we completed the work.
+	CompletedOn time.Time `json:"completedOn,omitempty"`
+}
+
+func cpuinfo() (*NodeInfo, error) {
+	cpus, err := cpu.Info()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	j, err := json.Marshal(info)
+	hostInfo, err := host.Info()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	log.Printf("Host Info: %s", string(j))
+
+	return &NodeInfo{HostInfo: *hostInfo, CPUInfo: cpus}, nil
 }
 
 func main() {
-	cpuinfo()
-	showHostname()
+	ni, err := cpuinfo()
+	if err != nil {
+		log.Fatalf("cannot get node or cpu info: %v", err)
+	}
+	log.Printf("Node Info: %#v", ni)
+
 	starting := big.NewInt(0)
 	starting.SetBit(starting, 67, 1)
 	starting.SetBit(starting, 0, 1) // make odd
 	counter := 1
 	for {
 		counter++
-		if counter == 1000000 {
+		if counter == 10000000 {
 			log.Printf("bitlen %d testing %s", starting.BitLen(), starting)
 			counter = 1
 		}
@@ -59,10 +140,12 @@ func main() {
 
 }
 
-func iterate(s *big.Int) (iterCount int, n *big.Int) {
-	n = big.NewInt(0)
+func iterate(s *big.Int) int {
+	n := big.NewInt(0)
 	n.Add(n, s)
+	iterCount := 0
 	for {
+		iterCount++
 		if n.Bit(0) == 0 {
 			n.Rsh(n, 1)
 		} else {
@@ -73,7 +156,7 @@ func iterate(s *big.Int) (iterCount int, n *big.Int) {
 		if c == 0 {
 			log.Printf("Found a loop back to starting value: %s", n)
 		} else if c == -1 {
-			return
+			return iterCount
 		}
 	}
 }
